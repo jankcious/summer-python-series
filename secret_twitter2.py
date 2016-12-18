7bf3ad9af9a12857d589e5d723a18a7d47353df9
Jason Becker <jason.becker86@gmail.com>
2016-12-18 16:59:49 -0600

    tweaks

---
+++ secret_twitter2.py
@@ -0,0 +1,17 @@
+"""Twitter Key Library"""
+#import win32com.client.CLSIDToClass, pythoncom
+#import win32com.client.util
+#from pywintypes import IID
+#from win32com.client import Dispatch
+
+# The following 3 lines may need tweaking for the particular server
+# Candidates are pythoncom.Missing, .Empty and .ArgNotFound
+#defaultNamedOptArg=pythoncom.Empty
+#defaultNamedNotOptArg=pythoncom.Empty
+#defaultUnnamedArg=pythoncom.Empty
+
+#class constants:
+consumer_key = 'uogcmzlRpoBtyLOWQ5XpHhm2K'
+consumer_secret = 'giIPl0urdk9mJd8as7D15jGpghENrck9NB4ZUmFlJBsJhpq7SR'
+access_token_key = '217281852-EVDAcue1T6i2uHjWMfEA0OyFmESXPkN1OpX7QtXW'
+access_token_secret = 'lxQABrVVzXRqXpNekD5Or4gncYeTDrgqBEIoA98jXukmu'	
